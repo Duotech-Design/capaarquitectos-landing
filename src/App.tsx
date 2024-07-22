8d578a107a5971ede06ee6facf58255169027ad2
RA-1124 <alison.rangelperez@gmail.com>
2024-07-22 13:24:28 -0600

    animacion loading

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,18 @@
-
 import viteLogo from '/logo_capa.png'
 import './App.css'
+import './index.css' // Asegúrate de importar el archivo CSS aquí
 
 function App() {
-
-
   return (
     <>
       <div>
         <a href="#" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
         </a>
-       
+        <div className="div">
+          <p id="h2">Loading....<span id="lol"></span></p>
+        </div>
       </div>
-     
     </>
   )
 }
