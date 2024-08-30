2d7941f7206d76ac5d25e9be5b7d5c9a50d3d335
RA-1124 <alison.rangelperez@gmail.com>
2024-08-30 13:36:08 -0600

    change star to svg

--- src/components/philosophy/Philosophy.tsx
+++ src/components/philosophy/Philosophy.tsx
@@ -5,7 +5,7 @@ const Philosophy = () => {
     <div className="bg-philosophy-bg bg-cover bg-center bg-fixed text-white p-20 min-h-screen">
       <div className="max-w-full mx-auto grid gap-10 mt-28 ">
         <div className="relative pl-20">
-          <img src="/src/assets/svg/icons/star.png" alt="Star Icon" className="absolute top-0 left-0 h-8 w-8"/>
+          <img src="/src/assets/svg/icons/star.svg" alt="Star Icon" className="absolute top-0 left-0 h-8 w-8"/>
           <p className="leading-relaxed text-l">
             En CAPA Arquitectos, creemos que la arquitectura va más allá de la proyección y construcción de espacios;
             es la creación de experiencias y la materialización de sueños. Nos dedicamos a diseñar y construir proyectos
@@ -14,7 +14,7 @@ const Philosophy = () => {
         </div>
 
         <div className="relative pl-24">
-          <img src="/src/assets/svg/icons/star.png" alt="Star Icon" className="absolute top-0 left-0 h-8 w-8"/>
+          <img src="/src/assets/svg/icons/star.svg" alt="Star Icon" className="absolute top-0 left-0 h-8 w-8"/>
           <p className="leading-relaxed text-l">
             Nos especializamos en el diseño habitacional y comercial, remodelaciones y diseño de interiores, siempre 
             utilizando materiales en tendencia para asegurar la calidad y la innovación en cada proyecto. Nuestro 
@@ -24,7 +24,7 @@ const Philosophy = () => {
         </div>
 
         <div className="relative pl-28">
-          <img src="/src/assets/svg/icons/star.png" alt="Star Icon" className="absolute top-0 left-0 h-8 w-8"/>
+          <img src="/src/assets/svg/icons/star.svg" alt="Star Icon" className="absolute top-0 left-0 h-8 w-8"/>
           <p className="leading-relaxed text-l">
             En cada proyecto, nos esforzamos por crear espacios funcionales, estéticamente atractivos y sostenibles, 
             que no solo cumplan con los estándares más altos de la industria, sino que también superen las expectativas 
