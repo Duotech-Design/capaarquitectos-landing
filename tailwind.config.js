1ca618c56260641b7d19b54a21823d4d0b76cd16
Daniel Escareno <daniel.esc@outlook.com>
2024-09-07 20:22:52 -0600

    optimize images to webp

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {
       colors: {
@@ -13,14 +10,15 @@ export default {
         darkBlue: "#213444",
       },
       backgroundImage: {
-        'custom-img': "url('https://img.daisyui.com/images/stock/photo-1507358522600-9f71e620c44e.webp')",
-        'member-cofundador': "url('https://pagedone.io/asset/uploads/1696238374.png')",
-        'contactUs-bg': "url('src/assets/img/contact-us-bg.jpg')",
-        'home-bg': "url('src/assets/img/home-bg.jpg')",
-        'home-bg-crop': "url('src/assets/img/home-bg-crop.jpg')",
+        "custom-img":
+          "url('https://img.daisyui.com/images/stock/photo-1507358522600-9f71e620c44e.webp')",
+        "member-cofundador":
+          "url('https://pagedone.io/asset/uploads/1696238374.png')",
+        "contactUs-bg": "url('src/assets/img/contact-us-bg.webp')",
+        "home-bg": "url('src/assets/img/home-bg.webp')",
+        "home-bg-crop": "url('src/assets/img/home-bg-crop.webp')",
       },
     },
   },
   plugins: [],
-}
-
+};
