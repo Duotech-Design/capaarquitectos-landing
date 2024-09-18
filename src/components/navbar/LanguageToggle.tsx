092d7bc3cf403e06981a6de83a1ae97583068725
RA-1124 <alison.rangelperez@gmail.com>
2024-09-18 13:33:40 -0600

    changes in translation

--- src/components/navbar/LanguageToggle.tsx
+++ src/components/navbar/LanguageToggle.tsx
@@ -1,10 +1,10 @@
 import { useTranslation } from "react-i18next";
-
 import { useState } from "react";
 
 const LanguageToggle = () => {
   const { t, i18n } = useTranslation("global");
-  const [dwopdownOpen, setDropdownOpen] = useState(false);
+  const [dropdownOpen, setDropdownOpen] = useState(false);
+  const currentLanguage = i18n.language; // Obtener el idioma actual
 
   const handleChange = (option: string) => {
     i18n.changeLanguage(option);
@@ -17,13 +17,16 @@ const LanguageToggle = () => {
         className="inline-flex justify-center w-full rounded-md px-3 py-2 text-sm font-medium text-white hover:text-white focus:outline-none"
         id="options-menu"
         aria-haspopup="true"
-        aria-expanded={dwopdownOpen}
-        onClick={() => setDropdownOpen(!dwopdownOpen)}
+        aria-expanded={dropdownOpen}
+        onClick={() => setDropdownOpen(!dropdownOpen)}
       >
-        EN/ES
+        {/* Añadimos espacio entre EN y ES con mx-2 */}
+        <span className={`${currentLanguage === 'en' ? 'underline' : ''} mx-2`}>EN</span>
+        /
+        <span className={`${currentLanguage === 'es' ? 'underline' : ''} mx-2`}>ES</span>
         <svg
           className={`ml-2 -mr-1 h-5 w-5 transition-transform duration-300 ${
-            dwopdownOpen ? "rotate-180" : "rotate-0"
+            dropdownOpen ? "rotate-180" : "rotate-0"
           }`}
           xmlns="http://www.w3.org/2000/svg"
           viewBox="0 0 20 20"
@@ -39,7 +42,7 @@ const LanguageToggle = () => {
       </button>
       <div
         className={`${
-          dwopdownOpen
+          dropdownOpen
             ? "fade-in origin-top-right absolute right-0 mt-2 w-32 rounded-md shadow-lg bg-black backdrop-blur-sm ring-1 ring-black ring-opacity-5"
             : "hidden"
         }`}
@@ -50,14 +53,22 @@ const LanguageToggle = () => {
         <div className="py-1" role="none">
           <button
             onClick={() => handleChange("en")}
-            className="block w-full text-left px-4 py-2 text-sm text-white hover:bg-darkGray"
+            className={`block w-full text-left px-4 py-2 text-sm hover:bg-darkGray ${
+              currentLanguage === "en"
+                ? "text-white font-cocoUltralight underline decoration-white"
+                : "text-white"
+            }`}
             role="menuitem"
           >
             {t("navbar.english")}
           </button>
           <button
             onClick={() => handleChange("es")}
-            className="block w-full text-left px-4 py-2 text-sm text-white hover:bg-darkGray"
+            className={`block w-full text-left px-4 py-2 text-sm hover:bg-darkGray ${
+              currentLanguage === "es"
+                ? "text-white font-cocoUltralight underline decoration-white"
+                : "text-white"
+            }`}
             role="menuitem"
           >
             {t("navbar.spanish")}
