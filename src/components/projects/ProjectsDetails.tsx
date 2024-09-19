59a80c31526cf488fde66b7902a0fb22ca6914fb
RA-1124 <alison.rangelperez@gmail.com>
2024-09-19 14:22:40 -0600

    still making changes in detail proyects

--- src/components/projects/ProjectsDetails.tsx
+++ src/components/projects/ProjectsDetails.tsx
@@ -23,7 +23,6 @@ const ProjectsDetails = () => {
   const nextProject = () => {
     const nextProject = projectList.indexOf(id) + 1;
     navigate(`/project/${projectList[nextProject]}`);
-    
   };
 
   const prevProject = () => {
@@ -34,53 +33,55 @@ const ProjectsDetails = () => {
   return (
     <section className="flex flex-col mx-auto px-2 sm:px-6 lg:px-8 min-h-screen max-w-[2000px]">
       <header className="flex justify-center md:justify-start">
-        <h3 className="mt-28 pb-6 md:pb-1 lg:text-2xl text-sm drop-shadow-sm text-black flex justify-between items-center gap-x-3 w-[300px]">
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            className={`w-7 h-7 rotate-180 ${
-              currentProjectIndex < 1
-                ? "disabled text-transparent"
-                : "text-black hover:text-gray-500 cursor-pointer animate-pulse"
-            }`}
-            fill="none"
-            viewBox="0 0 24 24"
-            stroke="currentColor"
-            strokeWidth={1}
-            onClick={currentProjectIndex > 0 ? prevProject : undefined}
-          >
-            <path
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              d="M13 5l7 7-7 7M5 5l7 7-7 7"
-            />
-          </svg>
-          <span className="font-sans font-extralight text-base text-gray-500">
-            {projectName}
-          </span>
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            className={`w-7 h-7 ${
-              currentProjectIndex === projectList.length - 1
-                ? "disabled text-transparent"
-                : "text-black hover:text-gray-500 cursor-pointer animate-pulse"
-            }`}
-            fill="none"
-            viewBox="0 0 24 24"
-            stroke="currentColor"
-            strokeWidth={1}
-            onClick={
-              currentProjectIndex < projectList.length - 1
-                ? nextProject
-                : undefined
-            }
-          >
-            <path
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              d="M13 5l7 7-7 7M5 5l7 7-7 7"
-            />
-          </svg>
-        </h3>
+      <h3 className="mt-28 pb-6 md:pb-1 lg:text-2xl text-sm drop-shadow-sm text-black flex justify-between items-center gap-x-3 w-[400px]"> {/* Cambiado de w-[300px] a w-[400px] */}
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    className={`w-7 h-7 rotate-180 ${
+      currentProjectIndex < 1
+        ? "disabled text-transparent"
+        : "text-black hover:text-gray-500 cursor-pointer animate-pulse"
+    }`}
+    fill="none"
+    viewBox="0 0 24 24"
+    stroke="currentColor"
+    strokeWidth={1}
+    onClick={currentProjectIndex > 0 ? prevProject : undefined}
+  >
+    <path
+      strokeLinecap="round"
+      strokeLinejoin="round"
+      d="M13 5l7 7-7 7M5 5l7 7-7 7"
+    />
+  </svg>
+  <span className="font-cocoUltralight text-lg lg:text-2xl text-gray-500 mt-6">
+  {projectName}
+</span>
+<svg
+  xmlns="http://www.w3.org/2000/svg"
+  className={`w-7 h-7 mt-6 ${
+    currentProjectIndex === projectList.length - 1
+      ? "disabled text-transparent"
+      : "text-black hover:text-gray-500 cursor-pointer animate-pulse"
+  }`}
+  fill="none"
+  viewBox="0 0 24 24"
+  stroke="currentColor"
+  strokeWidth={1}
+  onClick={
+    currentProjectIndex < projectList.length - 1
+      ? nextProject
+      : undefined
+  }
+>
+  <path
+    strokeLinecap="round"
+    strokeLinejoin="round"
+    d="M13 5l7 7-7 7M5 5l7 7-7 7"
+  />
+</svg>
+
+</h3>
+
       </header>
 
       <div className="text-sm flex justify-center pb-6 lg:pb-3 2xl:pt-20 gap-x-3 md:gap-x-12 w-full">
@@ -93,7 +94,7 @@ const ProjectsDetails = () => {
 
       <div className="grid grid-cols-1 md:grid-cols-12 gap-y-2 md:gap-y-1">
         <div className=" lg:col-span-3 md:col-span-2 text-sm lg:text-base text-right lg:pr-3">
-          {projectName}
+          {/* {projectName} */}
         </div>
         <div className="col-span-10 lg:col-span-6 md:col-span-8 text-focus-in">
           <SimpleSlider id={id} />
