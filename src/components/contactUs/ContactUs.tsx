ea5fdd1f7afa8dcc45a0a2afcf1b634e47bd7537
ZoiloMendoza <zoilomm@hotmail.com>
2024-09-23 20:11:45 -0600

    contact-us check

--- src/components/contactUs/ContactUs.tsx
+++ src/components/contactUs/ContactUs.tsx
@@ -2,6 +2,7 @@ import Form from "./Form";
 import { useState } from "react";
 import { useTranslation } from "react-i18next";
 import ContactBg from "@/assets/img/contact-us-bg.webp";
+
 const ContactUs = () => {
   const { t } = useTranslation("global");
   const [isImageLoaded, setIsImageLoaded] = useState(false);
@@ -11,25 +12,24 @@ const ContactUs = () => {
   };
 
   return (
-    <section className="overflow-hidden flex flex-wrap justify-center md:justify-normal items-center md:items-baseline gap-0 mx-auto px-2 sm:px-6 lg:px-8  bg-darkBlue h-[900px] lg:h-[760px] 2xl:h-full ">
+    <section className="relative overflow-hidden flex flex-wrap justify-center md:justify-normal items-center md:items-baseline gap-0 mx-auto px-2 sm:px-6 lg:px-8 bg-darkBlue min-h-screen">
       <img
         src={ContactBg}
         alt="contact-us-bg"
         onLoad={handleImageLoad}
-        className="absolute inset-0 w-full h-[900px] lg:h-[760px] object-cover z-0 2xl:h-full"
+        className="absolute inset-0 w-full h-full object-cover z-0"
       />
       {!isImageLoaded && (
+      
         <div className="absolute inset-0 bg-transparent opacity-50 z-10 flex justify-center items-center">
-          <div className="spinner"></div>
-        </div>
+        <div className="spinner"></div>
+      </div>
       )}
-      <header className="mt-28 md:mt-32 h-fit">
-        <h3 className="drop-shadow-2xl text-2xl text-white text-focus-in alig font-cocoUltralight">
-          {t("form.title")}
-        </h3>
+      <header className="relative mt-28 md:mt-32 h-fit w-full">
+        <h1 className="drop-shadow-2xl text-2xl text-white text-focus-in alig font-cocoUltralight">{t("form.title")}</h1>
       </header>
-      <div className="flex-1 flex justify-center lg:justify-end">
-        <div className="mt-0 sm:mt-28 w-full md:w-10/12 lg:w-7/12 w-max-[550px] h-[900px] lg:h-[760px]">
+      <div className="relative flex-1 flex justify-center lg:justify-end pb-3">
+        <div className="w-full md:w-10/12 lg:w-12/12 max-w-[650px] h-full lg:h-auto">
           <Form />
         </div>
       </div>
