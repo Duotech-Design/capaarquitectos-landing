2b6189d905d94234631d8232c5702253019b9198
RA-1124 <alison.rangelperez@gmail.com>
2024-10-24 16:20:56 -0600

    changes style philosphy

--- src/components/philosophy/Philosophy.tsx
+++ src/components/philosophy/Philosophy.tsx
@@ -2,7 +2,6 @@ import Start from "@/assets/svg/icons/star.svg";
 import { useTranslation } from "react-i18next";
 import CustomButton from "../ui/CustomButton";
 import { useNavigate } from "react-router-dom";
-
 import "./Philosophy.css";
 
 const Philosophy = () => {
@@ -15,12 +14,18 @@ const Philosophy = () => {
 
   return (
     <div className="bg-philosophy-bg bg-cover bg-center bg-fixed">
-      <div className="inset-0 bg-darkGray opacity-65 z-0 flex flex-col px-2 sm:px-6 lg:px-8 text-white py-16 md:py-0 md:min-h-screen">
-        <h3 className="mt-20 md:mt-32 pb-1 md:pb-20 font-bold sm:font-thin text-sm md:text-xl lg:text-2xl 2xl:text-3xl drop-shadow-sm text-gray-200 text-focus-in">
-          {t("philosophy.title")}
-        </h3>
+      {/* Contenedor principal con padding igual en ambos lados solo en móvil */}
+      <div className="inset-0 bg-darkGray opacity-65 z-0 flex flex-col px-4 md:px-6 lg:px-8 text-white py-10 md:py-0 md:min-h-screen">
+        {/* Título */}
+        <div className="mt-20 md:mt-32 pb-1 md:pb-20 font-bold sm:font-thin text-base md:text-xl lg:text-2xl 2xl:text-3xl drop-shadow-sm text-gray-200 text-focus-in">
+          <h3>{t("philosophy.title")}</h3>
+        </div>
+
+        {/* Contenido principal */}
         <div className="grid grid-cols-1 md:grid-cols-12 gap-y-1 md:gap-y-4 2xl:gap-y-8">
           <div className="col-span-2"></div>
+
+          {/* Primer bloque de texto e imagen */}
           <div
             className="col-span-10 text-focus-in flex gap-1 text-wrap w-full"
             style={{ animationDelay: "0s" }}
@@ -35,7 +40,10 @@ const Philosophy = () => {
               {t("philosophy.description_p1")}
             </p>
           </div>
+
           <div className="col-span-3"></div>
+
+          {/* Segundo bloque de texto e imagen */}
           <div
             className="col-span-9 text-focus-in flex gap-1 text-wrap w-full"
             style={{ animationDelay: "0.5s" }}
@@ -50,7 +58,10 @@ const Philosophy = () => {
               {t("philosophy.description_p2")}
             </p>
           </div>
+
           <div className="col-span-4"></div>
+
+          {/* Tercer bloque de texto e imagen */}
           <div
             className="col-span-8 text-focus-in flex gap-1 text-wrap w-full"
             style={{ animationDelay: "1s" }}
@@ -66,8 +77,9 @@ const Philosophy = () => {
             </p>
           </div>
         </div>
-        {/* Ajuste para el botón */}
-        <div className="flex justify-end pt-4 md:pt-24 mr-10">
+
+        {/* Botón personalizado ajustado en móvil */}
+        <div className="flex justify-end pt-4 md:pt-24 pr-0 pl-4 md:pl-0">
           <CustomButton text={t("philosophy.button")} onClick={handleButtonClick} />
         </div>
       </div>
@@ -76,5 +88,3 @@ const Philosophy = () => {
 };
 
 export default Philosophy;
-
-
